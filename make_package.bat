932f5f59a901478ab176c8f78fa10bbdd970948b
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2013-03-10 22:38:34 +0900

    初期コミット

---
+++ make_package.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call mvn clean
+call mvn compile -Dmaven.test.skip=true
+call mvn package -Dmaven.test.skip=true
+call mvn source:jar -Dmaven.test.skip=true
+call mvn javadoc:jar -Dmaven.test.skip=true
+rem mvn javadoc:javadoc -Dencoding=UTF-8 -Dcharset=UTF-8 -Ddecoding=UTF-8 -Dmaven.test.skip=true
+
+pause
