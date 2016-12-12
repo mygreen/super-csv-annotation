8c119b28619684474a35e8215f98265cb7503e35
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2016-12-12 18:24:21 +0900

    cleanオプションを削除

--- make_site.bat
+++ make_site.bat
@@ -7,7 +7,7 @@ if NOT "%JAVA_HOME_8%" == "" (
     set JAVA_HOME="%JAVA_HOME_8%"
 )
 
-call mvn clean site -Dgpg.skip=true > target/site.log 2>&1 
+call mvn site -Dgpg.skip=true > target/site.log 2>&1 
 
 start target/site.log
 
