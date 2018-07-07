5d41d90e964e16fcc6ce6bbc3b9ebcdb043beffe
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2018-07-07 13:26:43 +0900

    ・BeanFactoryクラスで、非staticなクラスに対応。 ・SpringBeanのクラスで、Bean名称が異なり同じクラスのBeanが存在する場合に対応。

--- src/main/java/com/github/mygreen/supercsv/builder/DefaultBeanFactory.java
+++ src/main/java/com/github/mygreen/supercsv/builder/DefaultBeanFactory.java
@@ -1,6 +1,7 @@
 package com.github.mygreen.supercsv.builder;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 import java.util.Objects;
 
 import org.supercsv.exception.SuperCsvReflectionException;
@@ -10,25 +11,36 @@ import org.supercsv.util.BeanInterfaceProxy;
 /**
  * Beanのインスタンスを生成する標準のクラス。
  *
+ * @version 2.2
  * @since 2.0
  * @author T.TSUCHIE
  *
  */
 public class DefaultBeanFactory implements BeanFactory<Class<?>, Object> {
-    
+
     @Override
     public Object create(final Class<?> clazz) {
         Objects.requireNonNull(clazz, "clazz should not be null.");
-        
+
         try {
             if(clazz.isInterface()) {
                 return BeanInterfaceProxy.createProxy(clazz);
-                
+
             } else {
+
+                final Class<?> declaredClass = clazz.getDeclaringClass();
+
+                if(declaredClass != null && !Modifier.isStatic(clazz.getModifiers())) {
+                    // 非staticな内部クラスの場合
+                    Constructor<?> cons = clazz.getDeclaredConstructor(declaredClass);
+                    cons.setAccessible(true);
+                    return cons.newInstance((Object)null);
+                }
+
                 Constructor<?> cons = clazz.getDeclaredConstructor();
                 cons.setAccessible(true);
                 return cons.newInstance();
-                
+
             }
         } catch (ReflectiveOperationException  e) {
             throw new SuperCsvReflectionException(String.format("fail create Bean instance of '%s'", clazz.getName()), e);
---
+++ src/test/java/com/github/mygreen/supercsv/builder/SpringTest.java
@@ -0,0 +1,151 @@
+package com.github.mygreen.supercsv.builder;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.supercsv.prefs.CsvPreference;
+
+import com.github.mygreen.supercsv.builder.spring.UserCsv;
+import com.github.mygreen.supercsv.io.CsvAnnotationBeanReader;
+import com.github.mygreen.supercsv.io.CsvAnnotationBeanWriter;
+import com.github.mygreen.supercsv.localization.SpringMessageResolver;
+import com.github.mygreen.supercsv.validation.CsvExceptionConverter;
+
+/**
+ * SpringによるCSVの読み下記のテスト
+ *
+ * @since 2.0
+ * @author T.TSUCHIE
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:/SpringTestContext.xml")
+//@ContextConfiguration(classes=SpringTestConfig.class)
+public class SpringTest {
+
+    @Autowired
+    private SpringBeanFactory beanFactory;
+
+    @Autowired
+    private SpringMessageResolver messageResolver;
+
+    private BeanMappingFactory beanMappingFactory;
+
+    private CsvExceptionConverter exceptionConverter;
+
+    @Before
+    public void setUp() throws Exception {
+
+        this.beanMappingFactory = new BeanMappingFactory();
+        beanMappingFactory.getConfiguration().setBeanFactory(beanFactory);
+
+        this.exceptionConverter = new CsvExceptionConverter();
+        exceptionConverter.setMessageResolver(messageResolver);
+    }
+
+    @Test
+    public void testRead() throws Exception {
+
+        BeanMapping<UserCsv> beanMapping = beanMappingFactory.create(UserCsv.class);
+
+        String csv = "id,名前,ホームページ,ホームページ（予備）,E-mail"
+                + "\r\n" + "001,admin,http://mysite.com/admin.html,https://localhost/sub.html,admin@mysite.com"
+                + "\r\n" + "002,sample,http://mysite.com/sample.html,https://localhost/sub.html,sample@mysite.com"
+                + "\r\n" + "003,test,ftp://mysite.com/test.html,https://localhost/sub.html,test@mysite.com"
+                + "\r\n" + "004,test,http://mysite.com/test.html,abc,test@mysite.com"
+                + "\r\n" + "005,admin,http://mysite.com/admin.html,https://localhost/sub.html,admin_mysite.com"
+                ;
+
+        CsvAnnotationBeanReader<UserCsv> csvReader = new CsvAnnotationBeanReader<>(beanMapping,
+                new StringReader(csv), CsvPreference.STANDARD_PREFERENCE);
+        csvReader.setExceptionConverter(exceptionConverter);
+
+        List<UserCsv> list = csvReader.readAll(true);
+        csvReader.close();
+
+        csvReader.getErrorMessages().stream().forEach(System.out::println);
+
+        assertThat(csvReader.getErrorMessages()).hasSize(4)
+            .contains("[3行, 2列] : 項目「名前」の値（sample）は、存在しません。")
+            .contains("[4行, 3列] : 項目「ホームページ」の値（ftp://mysite.com/test.html）のプロトコル「ftp」はサポートしていません。")
+            .contains("[5行, 4列] : 項目「ホームページ（予備）」の値（abc）は、サブのホームページの値として不正です。")
+            .contains("[6行, 5列] : 項目「E-mail」の値（admin_mysite.com）は、メールアドレスの書式として不正です。")
+            ;
+
+        assertThat(list).hasSize(1);
+        assertThat(list.get(0)).hasFieldOrPropertyWithValue("id", "001");
+
+    }
+
+    @Test
+    public void testWrite() throws Exception {
+
+        BeanMapping<UserCsv> beanMapping = beanMappingFactory.create(UserCsv.class);
+
+        List<UserCsv> list = new ArrayList<UserCsv>();
+        list.add(new UserCsv().id("001").name("admin")
+                .homepage(new URL("http://mysite.com/admin.html"))
+                .homepageSub(new URL("https://localhost/sub.html"))
+                .email("admin@mysite.com"));
+
+        list.add(new UserCsv().id("002").name("sample")
+                .homepage(new URL("http://mysite.com/sample.html"))
+                .homepageSub(new URL("https://localhost/sub.html"))
+                .email("sample@mysite.com"));
+
+        list.add(new UserCsv().id("003").name("test")
+                .homepage(new URL("ftp://mysite.com/test.html"))
+                .homepageSub(new URL("https://localhost/sub.html"))
+                .email("test@mysite.com"));
+
+//        list.add(new UserCsv().id("004").name("test")
+//                .homepage(new URL("http://mysite.com/admin.html"))
+//                .homepageSub(new URL("mailto://localhost/sub.html"))
+//                .email("test@mysite.com"));
+
+        list.add(new UserCsv().id("005").name("admin")
+                .homepage(new URL("http://mysite.com/admin.html"))
+                .homepageSub(new URL("https://localhost/sub.html"))
+                .email("admin_mysite.com"));
+
+
+        StringWriter writer = new StringWriter();
+
+        CsvAnnotationBeanWriter<UserCsv> csvWriter = new CsvAnnotationBeanWriter<>(beanMapping,
+                writer, CsvPreference.STANDARD_PREFERENCE);
+        csvWriter.setExceptionConverter(exceptionConverter);
+
+        csvWriter.writeAll(list, true);
+        csvWriter.close();
+
+        csvWriter.getErrorMessages().stream().forEach(System.out::println);
+
+        assertThat(csvWriter.getErrorMessages()).hasSize(3)
+            .contains("[3行, 2列] : 項目「名前」の値（sample）は、存在しません。")
+            .contains("[4行, 3列] : 項目「ホームページ」の値（ftp://mysite.com/test.html）のプロトコル「ftp」はサポートしていません。")
+            .contains("[5行, 5列] : 項目「E-mail」の値（admin_mysite.com）は、メールアドレスの書式として不正です。")
+            ;
+
+        String csv = writer.toString();
+        String expected = "id,名前,ホームページ,ホームページ（予備）,E-mail"
+                + "\r\n" + "001,admin,http://mysite.com/admin.html,https://localhost/sub.html,admin@mysite.com"
+                + "\r\n";
+        System.out.println(csv);
+        assertThat(csv).isEqualTo(expected);
+
+
+
+    }
+}
